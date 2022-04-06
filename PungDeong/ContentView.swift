d8fc99afa16df8e2b1a3eb8f05cb0e394d1aba8f
김상현 <kshy1019@naver.com>
2022-04-06 14:37:50 +0900

    Initial Commit

---
+++ PungDeong/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PungDeong
+//
+//  Created by 김상현 on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PungDeong/PungDeongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PungDeongApp.swift
+//  PungDeong
+//
+//  Created by 김상현 on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct PungDeongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
